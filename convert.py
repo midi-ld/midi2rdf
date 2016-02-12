2fcb823beca02c76e35a7516727c61abf967f4a4
Albert Meroño-Peñuela <albert.meronyo@gmail.com>
2016-02-12 18:51:43 +0100

    Lossless rountrip worksoneoneone

--- convert.py
+++ convert.py
@@ -17,7 +17,7 @@ pattern = mid[filename.split('.')[0]]
 g.add((pattern, RDF.type, mid.Pattern))
 
 # Since we won't mess with RDF statement order, we'll have absolute ticks
-pattern_midi.make_ticks_abs()
+# pattern_midi.make_ticks_abs()
 
 for n_track in range(len(pattern_midi)):
     track = m['track' + str(n_track).zfill(2)] #So we can order by URI later
@@ -28,8 +28,11 @@ for n_track in range(len(pattern_midi)):
         event = m['track' + str(n_track).zfill(2) + '/event'+ str(n_event).zfill(4)]
         g.add((event, RDF.type, mid[(type(event_midi).__name__)]))
         g.add((track, mid.hasEvent, event))
-        # Save the 'tick' slot
+        # Save the 'tick' slot (shared among all events)
         g.add((event, mid.tick, Literal(event_midi.tick)))
+        # Save the 'channel' slot
+        if hasattr(event_midi, 'channel'):
+            g.add((event, mid.channel, Literal(event_midi.channel)))
         # Save any other slots the event may have
         for slot in event_midi.__slots__:
             g.add((event, mid[slot], Literal(getattr(event_midi, slot))))
