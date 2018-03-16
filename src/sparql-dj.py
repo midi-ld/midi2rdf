d1a85f48f45b4ca07bcd4570f4ecf1d5e602de00
Albert Meroño-Peñuela <albert.meronyo@gmail.com>
2018-03-16 13:08:46 +0100

    endpoint for midi mashup

--- src/sparql-dj.py
+++ src/sparql-dj.py
@@ -60,6 +60,30 @@ def mashup():
 
     return make_response('200')
 
+@app.route('/midimashup')
+def midimashup():
+    pattern1 = "<http://purl.org/midi-ld/pattern/" + request.args.get('hash1') + ">"
+    pattern2 = "<http://purl.org/midi-ld/pattern/" + request.args.get('hash2') + ">"
+
+    print pattern1
+    print pattern2
+
+    url = "http://virtuoso-midi.amp.ops.labs.vu.nl/sparql"
+    query ="PREFIX mid: <http://purl.org/midi-ld/midi#> CONSTRUCT { <newsong> a mid:Pattern ; mid:hasTrack ?track . <newsong> mid:format ?format . <newsong> mid:resolution ?resolution . ?track mid:hasEvent ?event . ?track a mid:Track . ?event a ?type . ?event ?property ?value . } WHERE { { " + pattern1 + " mid:hasTrack ?track . " + pattern1 + " mid:format ?format . " + pattern1 + " mid:resolution ?resolution . ?track mid:hasEvent ?event . ?event a ?type . ?event ?property ?value . FILTER (?track IN (" + tracklist + " <arbitrary>)) } UNION { " + pattern2 + " mid:hasTrack ?track . " + pattern2 + " mid:format ?format . " + pattern2 + " mid:resolution ?resolution . ?track mid:hasEvent ?event . ?event a ?type . ?event ?property ?value . FILTER (?track IN (<arbitrary>)) } }"
+
+    print query
+
+    data = {"query" : query}
+    headers = {"Accept" : "text/turtle"}
+    res = requests.get(url, params=data, headers=headers)
+
+    with open('static/midi/mashup.ttl', 'w') as dump:
+        dump.write(res.text)
+
+    rdf2midi('static/midi/mashup.ttl', 'static/midi/mashup.mid')
+
+    return send_from_directory(directory='static/midi', filename='mashup.mid')
+
 @app.after_request
 def add_header(r):
     """
